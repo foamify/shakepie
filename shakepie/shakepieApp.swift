ced0c6909067b1697de8e655057498620a6d7db4
damywise <25608913+damywise@users.noreply.github.com>
2025-03-25 16:01:36 +0700

    Initial Commit

---
+++ shakepie/shakepieApp.swift
@@ -0,0 +1,17 @@
+//
+//  shakepieApp.swift
+//  shakepie
+//
+//  Created by Ahmad Arif Aulia Sutarman on 25/03/25.
+//
+
+import SwiftUI
+
+@main
+struct shakepieApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
